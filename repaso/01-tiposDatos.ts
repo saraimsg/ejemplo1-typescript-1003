721e4f1ccbdbb66f7bf5e06cd5ce5e4494d0ba83
saraimsg <saraisolis.mg@gmail.com>
2022-09-10 09:58:12 -0500

    primer commit del proyecto

---
+++ repaso/01-tiposDatos.ts
@@ -0,0 +1,19 @@
+/*
+    let es definida unicamente para una funcion 
+    var es definida para funciones globales
+*/
+
+console.log('Hola Mundo!-pwa-1003');
+let nombre:string="sara";
+let cont:number=111;
+let acredits:boolean=false;
+
+let apaterno: number | string='juan';
+apaterno=238;
+
+var miVariable:string='Hola';
+/*La constante igual es unica y ya no cambia una vez definida*/
+const tem=234;
+
+
+
