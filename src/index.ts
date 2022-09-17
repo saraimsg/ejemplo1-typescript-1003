f56543d24b4bd50943e9d886301b14598200e2b8
saraimsg <saraisolis.mg@gmail.com>
2022-09-16 19:06:45 -0500

    Practica

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
 
-//Un programa que permita crear la formula general con resultado de x y y con clases 
 
-class formula{
-    a: number=1;
-    b: number=2;
-    c: number=-8;
-    x1: number;
-    x2: number;
-    public resultado(){
-        this.obtener();
-    }
-    private obtener(){
-        this.x1=Math.floor(-b+(b*b)-4(a)(c));
-    }
-    public impri(){
-        console.log(`Salio el valor ${this.x1}`);
-    }    
-}
-let forGeneral=new formula();
-forGeneral.resultado();
-forGeneral.impri();
 
